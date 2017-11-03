9df34e6cba241dcc9d6b729ad38f0c83a4b4492f
Raúl Otaño Hurtado <rulyotano@gmail.com>
2017-11-03 17:51:27 -0200

    Add dev tools

--- src/components/configureStore.js
+++ src/components/configureStore.js
@@ -1,9 +1,12 @@
 import { createStore } from 'redux'
 import appReducer from './_store'
 
+const reduxTools = process.env.NODE_ENV === 'production' || !window.__REDUX_DEVTOOLS_EXTENSION__ ? 
+                    ()=>undefined : window.__REDUX_DEVTOOLS_EXTENSION__
+
 /**This function is just for creating the store. It is good to be a function cos can be used for instance in tests*/
 const configureStore = ()=>{
-    const store = createStore(appReducer)
+    const store = createStore(appReducer,  reduxTools())
     return store
 }
 
