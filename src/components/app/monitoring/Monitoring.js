cd96b906f95856c5fa871d2680c54dd1bc36b387
André Gallacci <andre.gallacci@gmail.com>
2018-04-18 10:35:25 -0300

    Loading Button created

--- src/components/app/monitoring/Monitoring.js
+++ src/components/app/monitoring/Monitoring.js
@@ -1,15 +1,39 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Segment from "../segment/Segment";
+import LoadingButton from '../loadingButton/LoadingButton';
 
+class Monitoring extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = { isLoading: false }
+    }
 
-class Monitoring extends Component{
-    render(){
-        return(
-            <Segment title="Monitoring"/>
+    onClickLoadingButton = () => {
+
+        this.setState({isLoading : true});
+
+        let teste = () => {
+            console.log("Executado");
+            this.setState({isLoading : false});
+        }
+
+        setTimeout(teste, 2000);
+    }
+
+    render() {
+        return (
+            <Segment title="Monitoring">
+                <LoadingButton
+                    variant="raised"
+                    color="primary"
+                    onClick={this.onClickLoadingButton}
+                    isLoading={this.state.isLoading}>
+                    Login
+                </LoadingButton>
+            </ Segment>
         )
     }
 }
-
 
 export default Monitoring;
