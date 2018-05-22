92537fe75f6514a12360a61fc95b29f8510b7ed7
Raúl Otaño Hurtado <rulyotano@gmail.com>
2018-05-22 14:27:24 -0300

    close-talhao-from-map: fix merge error

--- src/components/common/select/Select.js
+++ src/components/common/select/Select.js
@@ -41,7 +41,7 @@ class Select extends PureComponent{
     constructor(props){
         super(props);
         this.state ={
-            value:this.props.initialValue || (multiple ? []:''),
+            value:this.props.initialValue || (this.props.multiple ? []:''),
             criterion:''
         }
     }
