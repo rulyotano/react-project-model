54a23078dd73fde6010cb97279abd0709ea36425
Caique Vallim <caiquevallim@hotmail.com>
2018-06-04 10:59:03 -0300

    Created a horizontal scrollbar

--- src/components/app/close-field/process/ProcessCloseFieldTable.js
+++ src/components/app/close-field/process/ProcessCloseFieldTable.js
@@ -1,8 +1,8 @@
 import React, {PureComponent} from 'react';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
-
-
+import { connect } from 'react-redux';
+import { forEach, isNaN, head } from 'lodash';
 import Table from '@material-ui/core/Table';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
@@ -13,49 +13,18 @@ import TablePagination from '@material-ui/core/TablePagination';
 
 const styles = theme => ({
     table: {
-        maxHeight: 250,
         border:"1px solid #ddd",
-        boxShadow:'1px 1px 2px 1px #ddd'
+        boxShadow:'1px 1px 2px 1px #ddd',
+        minWidth: 1020
     },
+    tableWrapper:{
+        overflowX: 'auto',
+    },
+    tableRow:{
+        maxHeight:'48px !important'
+    }
 });
-const columns = [
-    {key:'process', label:'Process', isNumeric:false},
-    {key:'operation', label:'Operation', isNumeric:false},
-    {key:'field', label:'Field', isNumeric:false},
-    {key:'fieldArea', label:'Field Area (acre)', isNumeric:true},
-    {key:'machineArea', label:'Machine Area (acre)', isNumeric:true},
-    {key:'date', label:'Date', isNumeric:false},
-    {key:'condition', label:'Condition', isNumeric:false},
-];
-let nextId = 1;
-const createData = (obj) =>{
-    return {id:nextId++, ...obj};
-};
-const data = [
-   createData({process:"Process A", operation:"Operation A", field:"2117", fieldArea:1547, machineArea:1487, date:"25/05/2018", condition:"Closed"}),
-   createData({process:"Process B", operation:"Operation B", field:"55", fieldArea:1135, machineArea:1101, date:"26/05/2018", condition:"Pendent"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process A", operation:"Operation A", field:"2117", fieldArea:1547, machineArea:1487, date:"25/05/2018", condition:"Closed"}),
-   createData({process:"Process B", operation:"Operation B", field:"55", fieldArea:1135, machineArea:1101, date:"26/05/2018", condition:"Pendent"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process A", operation:"Operation A", field:"2117", fieldArea:1547, machineArea:1487, date:"25/05/2018", condition:"Closed"}),
-   createData({process:"Process B", operation:"Operation B", field:"55", fieldArea:1135, machineArea:1101, date:"26/05/2018", condition:"Pendent"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-   createData({process:"Process C", operation:"Operation C", field:"78", fieldArea:589, machineArea:585, date:"26/04/2018", condition:"In process"}),
-
-];
-
+const columns = [];
 
 class ProcessCloseFieldTable extends PureComponent{
     constructor(props){
@@ -63,7 +32,7 @@ class ProcessCloseFieldTable extends PureComponent{
         this.state = {
             rowIdSelected:null,
             page:0,
-            rowsPerPage:window.innerHeight<=600 ? 8: window.innerHeight<= 768 ? 11:16
+            rowsPerPage:window.innerHeight<=600 ? 7: window.innerHeight<= 768 ? 10:15
         }
     }
     static contextTypes = {
@@ -80,58 +49,77 @@ class ProcessCloseFieldTable extends PureComponent{
     handleChangePage(event, page){
         this.setState({ page });
     };
+    componentWillMount(){
+
+        let { data } = this.props;
+        let { t } = this.context;
+        let qtdColumns = 0;
+        forEach(head(data), (value, key)=>{
+
+            qtdColumns++;
+            columns.push({key, label:t(key), isNumeric:!isNaN(value)});
+            if(qtdColumns >= 17)
+                return false;
+        })
+    }
 
     render(){
 
-        const { classes, children } = this.props;
+        const { classes, children, data=[] } = this.props;
         const { page, rowsPerPage, rowIdSelected } = this.state;
         const { t } = this.context;
 
         const emptyRows = rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage);
 
         return(
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        {columns.map(h=><TableCell key={h.key} numeric={h.isNumeric}>{t(h.label)}</TableCell> )}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(i=>
-                        <TableRow key={i.id}
-                                  hover onClick={event => this.selectRow(event, i.id)}
-                                  selected={this.isSelected(i.id)}>
-                            {columns.map(h=><TableCell numeric={h.isNumeric} key={h.key}>{i[h.key]}</TableCell>)}
-                        </TableRow>
-                    )}
-                    {emptyRows > 0 && (
-                        <TableRow style={{ height: 48 * emptyRows }}>
-                            <TableCell colSpan={columns.length} />
-                        </TableRow>
-                    )}
-                </TableBody>
-                <TableFooter>
-                    <TableRow>
-                        <TableCell colSpan={6}>
-                            {children && rowIdSelected > 0 &&(
-                                children
-                            )}
-                        </TableCell>
-                        <TablePagination
-                            component={TableCell}
-                            labelDisplayedRows={({ from, to, count }) => `${from}-${to} ${t('of')} ${count}`}
-                            rowsPerPageOptions={[]}
-                            count={data.length}
-                            rowsPerPage={rowsPerPage}
-                            page={page}
-                            backIconButtonProps={{'aria-label': t('Previous Page'),}}
-                            nextIconButtonProps={{'aria-label': t('Next Page'),}}
-                            onChangePage={(event, page)=>this.handleChangePage(event, page)}
-                        />
+            <div className={classes.tableWrapper}>
 
-                    </TableRow>
-                </TableFooter>
-            </Table>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            {columns.map(h=><TableCell key={h.key} numeric={h.isNumeric}>{t(h.label)}</TableCell> )}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+
+                        {data.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map(i=>
+                            <TableRow key={i.cdId}
+                                      hover onClick={event => this.selectRow(event, i.cdId)}
+                                      selected={this.isSelected(i.cdId)}
+                                      className={classes.tableRow}>
+                                {columns.map(h=><TableCell numeric={h.isNumeric} key={h.key}>{i[h.key]}</TableCell>)}
+                            </TableRow>
+                        )}
+                        {emptyRows > 0 && (
+                            <TableRow style={{ height: 48 * emptyRows }}>
+                                <TableCell colSpan={columns.length} />
+                            </TableRow>
+                        )}
+                    </TableBody>
+                    <TableFooter>
+                        <TableRow>
+                            <TableCell colSpan={6}>
+                                {children && rowIdSelected > 0 &&(
+                                    children
+                                )}
+                            </TableCell>
+                            <TablePagination
+                                component={TableCell}
+                                style={{position:"absolute", right:0}}
+                                labelDisplayedRows={({ from, to, count }) => `${from}-${to} ${t('of')} ${count}`}
+                                rowsPerPageOptions={[]}
+                                count={data.length}
+                                rowsPerPage={rowsPerPage}
+                                page={page}
+                                backIconButtonProps={{'aria-label': t('Previous Page')}}
+                                nextIconButtonProps={{'aria-label': t('Next Page')}}
+                                onChangePage={(event, page)=>this.handleChangePage(event, page)}
+                            />
+
+                        </TableRow>
+                    </TableFooter>
+                </Table>
+            </div>
         )
     }
 }
@@ -139,5 +127,9 @@ class ProcessCloseFieldTable extends PureComponent{
 ProcessCloseFieldTable.propTypes = {
     classes: PropTypes.object.isRequired,
 };
+const mapStateToProps = (state) => ({
+    data: state.app.closeField.process.data,
+});
 
-export default withStyles(styles)(ProcessCloseFieldTable);
+
+export default connect(mapStateToProps)(withStyles(styles)(ProcessCloseFieldTable));
